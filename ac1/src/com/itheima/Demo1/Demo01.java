2b7660afc9a28c3181b6c03839a49a3c3e0114c1
itcast <itcast@qq.com>
2018-12-23 21:13:49 +0800

    第三次

---
+++ ac1/src/com/itheima/Demo1/Demo01.java
@@ -0,0 +1,10 @@
+package com.itheima.Demo1;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+    }
+}
